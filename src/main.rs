4aedf3201e52671c8d515eb3fd3b57201eb9a87e
symphony <jciurc@gmail.com>
2025-03-31 00:17:00 -0700

    set up slint app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-mod calculator;
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    calculator::run();
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    ui.run()?;
+
+    Ok(())
 }
