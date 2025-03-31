62acb7e918ae2742741455151aee95bded72fa9c
jciurc <jciurc@gmail.com>
2025-03-31 00:57:19 -0700

    set up for web deployment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 slint::include_modules!();
 
-fn main() {
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn main() {
     let main_window = MainWindow::new().unwrap();
 
     main_window.run().unwrap();
