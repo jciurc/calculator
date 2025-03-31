7b94088215da823cbe5582771f006c9e69dbdfbe
jciurc <jciurc@gmail.com>
2025-03-30 19:14:34 -0700

    move calculator into module

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
-use std::io::{Write, stdin, stdout};
-
-fn read(prompt: &str) -> String {
-    let mut input = String::new();
-
-    print!("{prompt}");
-    stdout().flush().expect("failed to flush");
-    stdin().read_line(&mut input).expect("failed to read");
-    input
-}
+mod calculator;
 
 fn main() {
-    println!("== Calculator ==");
-    println!("----------------");
-
-    let x = read("\nx: ");
-    let y = read("y: ");
-    let operator = read("Operator [+-*/]: ");
-
-    let x: f32 = x.trim().parse().unwrap();
-    let y: f32 = y.trim().parse().unwrap();
-    let operator: char = operator.trim().chars().next().unwrap();
-
-    let result = match operator {
-        '+' => x + y,
-        '-' => x - y,
-        '*' => x * y,
-        '/' => x / y,
-        _ => {
-            println!("Operator not recognized");
-            return;
-        }
-    };
-
-    println!("{x} {operator} {y} = {result}");
+    calculator::run();
 }
