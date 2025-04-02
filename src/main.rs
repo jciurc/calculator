80e7f884b16a643715b923efab83b2cbbc2fd2b4
jciurc <jciurc@gmail.com>
2025-04-01 23:25:10 -0700

    add parenthesis

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ pub fn main() {
     let ui = MainWindow::new().unwrap();
     let ui_handle = ui.as_weak();
 
-    let mut results: String = ui.get_results().to_string();
+    let mut result: String = ui.get_result().to_string();
     let mut display: String = ui.get_display().to_string();
+    let mut open_count = 0;
 
     ui.on_callback(move |key| {
         let ui = ui_handle.unwrap();
@@ -17,7 +18,7 @@ pub fn main() {
         // build main string
         match key {
             // clear leading 0
-            '0'..='9' if display == "0" => display.clear(),
+            '0'..='9' | '(' | '-' if display == "0" => display.clear(),
             _ => {}
         }
 
@@ -29,13 +30,22 @@ pub fn main() {
                 }
             }
             '-' | '+' | '*' | '/' | '%' | '^' => {
-                if display.len() > 0 && !"-+*/%^".contains(display.chars().last().unwrap()) {
+                if display.len() > 0 && !"(-+*/%^".contains(display.chars().last().unwrap()) {
                     display.push(key)
                 }
             }
+            '(' => {
+                open_count += 1;
+                display.push(key);
+            }
+            ')' => {
+                if open_count > 0 && !"(-+*/%^.".contains(display.chars().last().unwrap()) {
+                    open_count -= 1;
+                    display.push(key);
+                }
+            }
             'C' => display = '0'.to_string(),
-            '=' => {}
-            _ => display.push(key),
+            _ => {}
         }
 
         ui.set_display(display.to_shared_string());
@@ -43,22 +53,22 @@ pub fn main() {
         // build result string
         match key {
             // clear leading 0
-            '0'..='9' if results == "0" => results.clear(),
+            '0'..='9' | '(' | '-' if result == "0" => result.clear(),
             _ => {}
         }
 
         match key {
-            '0'..='9' => results.push(key),
+            '0'..='9' => result.push(key),
             '.' => {
-                if !results.contains('.') {
-                    results.push(key);
+                if !result.contains('.') {
+                    result.push(key);
                 }
             }
             '=' => {}
-            _ => results.clear(),
+            _ => result.clear(),
         }
 
-        ui.set_results(results.to_shared_string());
+        ui.set_result(result.to_shared_string());
     });
 
     ui.run().unwrap();
