4efb412b14fe3080427cea82bffc01748fcd4ae5
symphony <jciurc@gmail.com>
2025-04-01 18:04:40 -0700

    update styling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,5 @@ slint::include_modules!();
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
 pub fn main() {
-    let main_window = MainWindow::new().unwrap();
-
-    main_window.run().unwrap();
+    MainWindow::new().unwrap().run().unwrap();
 }
