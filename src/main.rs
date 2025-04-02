815abc7e895fb687021f4431dcda04e1b085e027
jciurc <jciurc@gmail.com>
2025-04-01 19:35:33 -0700

    set up buttons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use slint::ToSharedString;
+
 slint::include_modules!();
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
 pub fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    let ui = MainWindow::new().unwrap();
+    let ui_handle = ui.as_weak();
+
+    ui.on_callback(move |value| {
+        let ui = ui_handle.unwrap();
+        ui.set_display((ui.get_display() + &value.to_shared_string()).into())
+    });
+
+    ui.run().unwrap();
 }
