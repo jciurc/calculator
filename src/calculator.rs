d90465a147926b8c2870ddbf79717d87c6e0b92b
jciurc <jciurc@gmail.com>
2025-08-15 01:52:29 -0700

    refactor parsing

--- src/calculator.rs
+++ src/calculator.rs
@@ -17,35 +17,42 @@ pub fn calculate(expr: String) -> String {
 
     for char in expr.chars() {
         match char {
+            // build operand
             '0'..='9' | '.' => operand.push(char),
-            '-' if operand.len() == 0 => operand.push(char),
-            _ if precedence(char) > 0 => {
-                if operand.len() > 0 {
-                    output.push(operand.clone());
-                    operand.clear();
+            '-' if operand.is_empty() => operand.push(char),
+            // push operand
+            '+' | '-' | '*' | '/' | '%' | '^' | '(' | ')' if operand.len() > 0 => {
+                output.push(operand.clone());
+                operand.clear();
+
+                if char == '(' {
+                    stack.push('*');
                 }
+            }
+            // skip illegal characters
+            _ => continue,
+        }
+
+        // handle operators
+        match char {
+            _ if precedence(char) > 0 => {
                 while stack.len() > 0 && precedence(stack[stack.len() - 1]) < precedence(char) {
                     output.push(stack.pop().unwrap().to_string());
                 }
-                stack.push(char);
-            }
-            '(' => {
-                if operand.len() > 0 {
-                    output.push(operand.clone());
-                    operand.clear();
-                    stack.push('*');
+                if char == '-' {
+                    output.push("-1".to_string());
+                    stack.push('+');
+                    stack.push('/');
+                } else {
+                    stack.push(char);
                 }
-                stack.push(char);
             }
+            '(' => stack.push(char),
             ')' => {
-                if operand.len() > 0 {
-                    output.push(operand.clone());
-                    operand.clear();
-                }
                 while stack.len() > 0 && stack[stack.len() - 1] != '(' {
                     output.push(stack.pop().unwrap().to_string());
                 }
-                if stack.len() > 0 && stack[stack.len() - 1] == '(' {
+                if stack.len() > 0 {
                     stack.pop();
                 }
             }
